45974808bcad391bdec4e325b548658644eec2ec
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-19 18:31:10 -0700

    Time: 393 ms (5.1%), Space: 13 MB (19.82%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -11,17 +11,14 @@ class Solution(object):
 
         while low <= high:
             center = int(floor((low+high)/2))
-            print(center)
-            print(low, high)
+ 
             if target == nums[center]:
                 return center
-
+                
             if target > nums[center]:
                 low = center + 1
             else:
                 high = center - 1
 
-        print(center)
-        print(low, high)
         return -1 if nums[center] != target else center
 
