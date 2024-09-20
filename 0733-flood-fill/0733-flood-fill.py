0d30c46f43038d254ab7872adbc900b1a042faf3
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-19 19:42:49 -0700

    Time: 77 ms (9.63%), Space: 11.9 MB (30.95%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+
+        dfs = [[sr,sc]]
+        orig = image[sr][sc]
+        retImg = image
+
+        # Edge Cases:
+        if color == orig:
+            return retImg
+
+        # validate that sr sc in bounds
+        if sr > len(image)-1 or sr < 0 or sc > len(image)-1 or sc < 0:
+            return retImg
+
+        while len(dfs) != 0:
+            x,y = dfs.pop(0)
+            print([x,y],dfs)
+
+            if x > len(image)-1 or x < 0 or y > len(image[0])-1 or y < 0:
+                continue
+
+            if retImg[x][y] == orig:
+                retImg[x][y] = color
+                print("paint")
+                dfs.append([x+1,y])
+                dfs.append([x-1,y])
+                dfs.append([x,y+1])
+                dfs.append([x,y-1])
+                
+            
+        return retImg
+
+
