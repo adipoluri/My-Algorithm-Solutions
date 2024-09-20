702f9de63f18e60db22a72446a93877de65a1afd
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-19 18:30:08 -0700

    Time: 382 ms (8.15%), Space: 12.8 MB (53.16%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        high = len(nums)-1
+        low = 0
+        center = low+int((low+high)/2)
+
+        while low <= high:
+            center = int(floor((low+high)/2))
+            print(center)
+            print(low, high)
+            if target == nums[center]:
+                return center
+
+            if target > nums[center]:
+                low = center + 1
+            else:
+                high = center - 1
+
+        print(center)
+        print(low, high)
+        return -1 if nums[center] != target else center
+
