262143540442b3a8a40c67944fdeaf04a8c656af
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-23 17:06:14 -0700

    Time: 32 ms (49.43%), Space: 11.6 MB (52.15%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,51 +1,34 @@
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+    "IV": 4,
+    "IX": 9,
+    "XL": 40,
+    "XC": 90,
+    "CD": 400,
+    "CM": 900,
+}
+
+
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        count = 0
-        prev = None
-        for symbol in s:
-            
-            if symbol == "I":
-                count += 1
-
-            elif symbol == "V":
-                if prev == "I":
-                    count += 3
-                else:
-                    count += 5
-
-            elif symbol == "X":
-                if prev == "I":
-                    count += 8
-                else:
-                    count += 10
-
-            elif symbol == "L":
-                if prev == "X":
-                    count += 30
-                else:
-                    count += 50
-
-            elif symbol == "C":
-                if prev == "X":
-                    count += 80
-                else:
-                    count += 100
-
-            elif symbol == "D":
-                if prev == "C":
-                    count += 300
-                else:
-                    count += 500
-
-            elif symbol == "M":
-                if prev == "C":
-                    count += 800
-                else:
-                    count += 1000
-
-            prev = symbol
-        return count
+        total = 0
+        i = 0
+        while i < len(s):
+            # This is the subtractive case.
+            if i < len(s) - 1 and s[i : i + 2] in values:
+                total += values[s[i : i + 2]]
+                i += 2
+            else:
+                total += values[s[i]]
+                i += 1
+        return total
