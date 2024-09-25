329d41a383d294d4ec7f34b20396395f41c2d3b3
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-24 18:08:54 -0700

    Time: 15 ms (53.41%), Space: 11.7 MB (50.45%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -5,6 +5,49 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        return self.answer2(s,t)
+
+    def answer2(self,s,t):
+        i,j = len(s)-1,len(t)-1
+        iBackspaces, jBackspaces = 0,0
+
+        while i >= 0 or j >= 0:
+            # Get index of next valid character
+            while i >= 0:
+                if s[i] == "#":
+                    iBackspaces += 1
+                    i -= 1
+                elif iBackspaces > 0:
+                    i -= 1
+                    iBackspaces -= 1
+                else:
+                    break
+
+            while j >= 0:
+                if t[j] == "#":
+                    jBackspaces += 1
+                    j -= 1
+                elif jBackspaces > 0:
+                    j -= 1
+                    jBackspaces -= 1
+                else:
+                    break
+
+            print(s[i],t[j])
+            if i>= 0 and j>= 0 and s[i] != t[j]:
+                return False
+            print(i,j)
+            if (i >= 0) != (j >= 0):
+                return False
+
+            i -= 1
+            j -= 1
+
+        return True
+
+    def answer1(self,s,t):
+        # Time: O(M+N)
+        # Space: O(M+N)
         stack1 = []
         stack2 = []
 
