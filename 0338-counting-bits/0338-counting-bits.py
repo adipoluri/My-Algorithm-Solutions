6210451fe7a473e24efbbb4c5168028d97cb9fd3
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-26 13:54:30 -0700

    Time: 47 ms (72.39%), Space: 15.5 MB (64.66%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
+        return self.sol2(n)
+
+
+    def sol1(self,n):
         if n == 0:
             return [0]
-            
+
         retVal = [0,1]
         curr = 1
         i = 2
@@ -15,5 +19,12 @@ class Solution(object):
                 curr = i
             retVal.append(retVal[i-curr]+1)
             i += 1
+
+        return retVal
+
+    def sol2(self,n):
+        retVal = [0]*(n+1)
+        for i in range(1,n+1):
+            retVal[i]=retVal[i & (i - 1)]+1
 
         return retVal
