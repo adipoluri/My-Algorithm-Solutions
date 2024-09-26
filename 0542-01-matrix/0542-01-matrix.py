30aba465fca559ffa4b11ae5d37b849b14d0da82
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-25 18:08:22 -0700

    Time: 607 ms (38.05%), Space: 14.2 MB (92.27%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,6 +4,33 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: List[List[int]]
         """
+        res = mat
+        bfs = []
+        for n in range(len(res)):
+            for m in range(len(res[0])):
+                if res[n][m] == 0:
+                    bfs.append([n,m,1])
+                else:
+                    res[n][m] = -1
+
+        directions = [[-1,0],[1,0],[0,-1],[0,1]]
+        while len(bfs) > 0:
+            n,m,dist = bfs.pop(0)
+            for dx, dy in directions:
+                next_row, next_col = n + dy, m + dx
+                
+                if next_row < 0 or next_row >= len(mat) or next_col < 0 or next_col >= len(mat[0]):
+                    continue
+
+                if res[next_row][next_col] == -1:
+                    res[next_row][next_col] = dist
+                    bfs.append([next_row,next_col,dist+1])
+                
+        return mat
+
+
+    def sol1(self,matInput):
+        mat = matInput
         bfs = []
         for n in range(len(mat)):
             for m in range(len(mat[0])):
