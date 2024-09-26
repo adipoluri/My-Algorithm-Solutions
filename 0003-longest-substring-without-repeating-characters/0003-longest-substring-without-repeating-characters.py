7affea1b435edc10993cb189f0c050ea391f0dd6
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-09-25 18:44:05 -0700

    Time: 37 ms (57.11%), Space: 12.4 MB (51.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,6 +5,20 @@ class Solution(object):
         :rtype: int
         """
         maxSubstring = 0
+        count = {}
+        curr = 0
+        
+        for i in range(len(s)):
+            if s[i] in count:
+                curr = max(count[s[i]],curr)
+      
+            maxSubstring = max(maxSubstring, i - curr + 1)
+            count[s[i]] = i + 1
+
+        return maxSubstring
+
+    def sol1(self,s):
+        maxSubstring = 0
         currSubstring = 0
         curr = 0
         count = {}
